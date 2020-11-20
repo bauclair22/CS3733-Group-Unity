e5fa8836c54e37fbba9a36c2a9b903d813632442
bauclair22 <59893555+bauclair22@users.noreply.github.com>
2020-11-20 16:27:01 -0500

    Created createChoiceRequest

---
+++ QuestionnaireApplication/src/httpRequestsAndResponses/createChoiceRequest.java
@@ -0,0 +1,36 @@
+package httpRequestsAndResponses;
+
+public class createChoiceRequest {
+	String title;
+	String description;
+	int numMembers;
+	String alternatives[];
+	String alternativeTitles[];
+	
+	public String getTitle() {return title;}
+	public void setTitle(String title) {this.title = title;}
+	
+	public String getDescription() {return description;}
+	public void setDescription(String description) {this.description = description;}
+	
+	public int getNumMembers() {return numMembers;}
+	public void setNumMembers(int numMembers) {this.numMembers = numMembers;}
+	
+	public String[] getAlternatives() {return alternatives;}
+	public void setAlternatives(String[] alternatives) {this.alternatives = alternatives;}
+	
+	public String[] getAlternativeTitles() {return alternativeTitles;}
+	public void setAlternativeTitles(String[] alternativeTitles) {this.alternativeTitles = alternativeTitles;}
+	
+	public createChoiceRequest(String title, String description, int numMembers, String[] alternatives,
+			String[] alternativeTitles) {
+		this.title = title;
+		this.description = description;
+		this.numMembers = numMembers;
+		this.alternatives = alternatives;
+		this.alternativeTitles = alternativeTitles;
+	}
+	public createChoiceRequest() {
+	}
+	
+}
