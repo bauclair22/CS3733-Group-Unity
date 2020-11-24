9e0de149ce58a84e2a055e3ac437c3fd51c39ae7
Hoang Nguyen <hoangtheshark2@gmail.com>
2020-11-24 18:31:24 -0500

    Added some js files to try to start working. I will be moving to another branch

--- QuestionnaireApplication2.0/js/api.js
+++ QuestionnaireApplication2.0/js/api.js
@@ -1,8 +1,6 @@
-/**
- * 
- */
+//this is the link from the API gateway after staging
 var base_url = "https://q5j6ufetwe.execute-api.us-east-1.amazonaws.com/Beta";
 
-//var add_url    = base_url + "calculator";   // POST
+var create_url; //   = base_url + "calculator";   // POST
 
 
