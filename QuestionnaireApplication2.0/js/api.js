d1a947eb00799012c66e64050231981e81252cec
Hoang Nguyen <hoangtheshark2@gmail.com>
2020-11-24 23:57:47 -0500

    Changed url

--- QuestionnaireApplication2.0/js/api.js
+++ QuestionnaireApplication2.0/js/api.js
@@ -2,7 +2,7 @@
 var base_url = "https://q5j6ufetwe.execute-api.us-east-1.amazonaws.com/Beta/";
 
 var create_choice_url = base_url + "/";   // POST
-var create_member_url = base_url + "/" + "/partipateChoice";   // POST
+var create_member_url = base_url + "/" + "partipateChoice";   // POST
 var choice_list_url = base_url + "/"; // GET
 
 
